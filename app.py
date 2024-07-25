49c19627a40451de31c5847547821c6306bfc0ce
prem1989 <anandprem1989@gmail.com>
2024-07-25 10:14:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    
     return 'Hello from Koyeb'
+    
+@app.route('/handle_post', methods=['POST'])
+def handle_post():
+    print request
 
 
 if __name__ == "__main__":
