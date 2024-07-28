d854e0adf828d09c9e22f2969b7e537e9ee9723c
prem1989 <anandprem1989@gmail.com>
2024-07-28 14:59:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import json
+from alice import placeOrder
+from alice import getScriptDetails
 app = Flask(__name__)
 
 @app.route('/')
