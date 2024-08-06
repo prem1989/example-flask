0660229acd7c60a82bd1a67e6ab63041e65ebe95
prem1989 <anandprem1989@gmail.com>
2024-08-06 09:34:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,15 +49,15 @@ def handle_post():
                 high=int(float(script.get('High')))
                 high = high+(ltp*0.001)
                 high = round(high,1)
-                target= round(high + (high*0.005),1)
-                stoploss= round(high - (high*0.005),1)
+                target= round((high*0.005),1)
+                stoploss= round((high*0.005),1)
                 placeBuyOrder(orderHeaders,script.get('TSymbl'),contract.get('token'),high,high,stoploss,target)
             if scanname=='RRR':
                 low=int(float(script.get('Low')))
                 low = low - (ltp*0.001)
                 low = round(low,1)
-                target= round(low - (low*0.005),1)
-                stoploss= round(low + (low*0.005),1)
+                target= round((low*0.005),1)
+                stoploss= round((low*0.005),1)
                 placeSellOrder(orderHeaders,script.get('TSymbl'),contract.get('token'),low,low,stoploss,target)
         
     # Return a success message
